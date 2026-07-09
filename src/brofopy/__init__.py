f7414bb40cf934b98e0d5f4b9f66f67cfe2082ed
martinvonk <vonk.mart@gmail.com>
2026-07-09 20:28:10 +0200

    update bronformat

--- src/brofopy/__init__.py
+++ src/brofopy/__init__.py
@@ -5,4 +5,4 @@ from brofopy.ext.hpd import to_obscollection
 
 from ._version import __version__, show_versions
 
-__all__ = ["read_bronformat", "__version__", "show_versions"]
+__all__ = ["read_bronformat", "__version__", "show_versions", "to_obscollection"]
